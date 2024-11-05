512c8502df88965772a6bc262624abf6891b92fa
Teo Risso <68182473+teorisso@users.noreply.github.com>
2024-11-05 18:55:01 -0300

    UPDATE CALIFICACION CONTROLLER

--- src/main/java/com/example/sistema_academico/controller/CalificacionController.java
+++ src/main/java/com/example/sistema_academico/controller/CalificacionController.java
@@ -1,21 +1,46 @@
 package com.example.sistema_academico.controller;
 
 import com.example.sistema_academico.entity.Calificacion;
+import com.example.sistema_academico.service.CalificacionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.sistema_academico.service.CalificacionService;
+import java.util.List;
 
 @RestController
-@RequestMapping("/calificacion")
+@RequestMapping("/calificaciones")
 public class CalificacionController {
 
     @Autowired
     private CalificacionService calificacionService;
 
-    @PostMapping("/add")
-    public void addCalificacion(@RequestBody Calificacion calificacion) {
-        calificacionService.addCalificacion(calificacion);
+    // Agregar una nueva calificación para un alumno específico
+    @PostMapping("/add/{legajo}")
+    public void addCalificacion(@PathVariable Integer legajo, @RequestBody Calificacion calificacion) {
+        calificacionService.addCalificacion(legajo, calificacion);
     }
 
+    // Eliminar todas las calificaciones de un alumno por su legajo
+    @DeleteMapping("/deleteByLegajo/{legajo}")
+    public void deleteCalificacionesPorLegajo(@PathVariable Integer legajo) {
+        calificacionService.deleteCalificacionesPorLegajo(legajo);
+    }
+
+    // Obtener todas las calificaciones de un alumno por su legajo
+    @GetMapping("/getByLegajo/{legajo}")
+    public List<Calificacion> getCalificacionesByLegajo(@PathVariable Integer legajo) {
+        return calificacionService.getCalificaciones(legajo);
+    }
+
+    // Obtener todas las calificaciones existentes
+    @GetMapping("/getAll")
+    public List<Calificacion> getAllCalificaciones() {
+        return calificacionService.getCalificaciones();
+    }
+
+    // Actualizar las calificaciones de un alumno específico
+    @PutMapping("/update/{legajo}")
+    public void updateCalificacion(@PathVariable Integer legajo, @RequestBody Calificacion nuevaCalificacion) {
+        calificacionService.updateCalificacion(legajo, nuevaCalificacion);
+    }
 }
